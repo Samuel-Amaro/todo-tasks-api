7f57a97585d83ded61ee172b77a459df4719b77c
Samuel Amaro <samuelamaro96746313@gmail.com>
2024-06-08 15:15:14 -0300

    feat: adiciona `endpoint get /api/v1/tasks`

--- src/routes/get-todos.ts
+++ src/routes/get-todos.ts
@@ -1,4 +1,8 @@
 import { Elysia, t } from "elysia";
+import { db } from "../database/connection";
+import { todo } from "../database/schema";
+import { asc } from "drizzle-orm";
 
-//TODO: IMPLEMENTAR HANDLER
-export const getAllTodos = new Elysia().get("/", () => "Get all todos");
+export const getAllTodos = new Elysia().get("/", async () => {
+  return await db.select().from(todo).orderBy(asc(todo.created_at));
+});
