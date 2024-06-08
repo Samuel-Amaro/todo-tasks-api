879c7982e3717a75209c28c32dba92c90d4f7211
Samuel Amaro <samuelamaro96746313@gmail.com>
2024-06-07 22:13:44 -0300

    feat: adiciona `endpoint create todo`

--- src/routes/create-todo.ts
+++ src/routes/create-todo.ts
@@ -1,11 +1,29 @@
 import { Elysia, t } from "elysia";
+import { db } from "../database/connection";
+import { todo } from "../database/schema/index";
 
-//TODO: IMPLEMENTAR HANDLER
 export const createTodo = new Elysia().post(
   "/",
-  ({ body }) =>
-    `created todo with title: ${body.title} and content: ${body.content}`,
+  async ({ body, set }) => {
+    if(body.title.trim()) {
+      await db.insert(todo).values({
+        title: body.title,
+        description: body.content,
+      });
+    }else{
+      set.status = 400
+      return {
+        code: "VALIDATION",
+        message: "Title não pode ser vazio"
+      }
+    }
+  },
   {
-    body: t.Object({ title: t.String(), content: t.String() }),
+    body: t.Object({
+      title: t.String({
+        minLength: 1
+      }),
+      content: t.String(),
+    }),
   }
 );
