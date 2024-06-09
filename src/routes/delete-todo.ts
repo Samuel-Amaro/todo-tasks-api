369e0a3c5e0c82500451b1b321ae99ca28b34b75
Samuel Amaro <samuelamaro96746313@gmail.com>
2024-06-09 13:35:45 -0300

    feat: adiciona `endpoint delete-todo`

--- src/routes/delete-todo.ts
+++ src/routes/delete-todo.ts
@@ -1,9 +1,13 @@
 import { Elysia, t } from "elysia";
+import { db } from "../database/connection";
+import { todo } from "../database/schema";
+import { eq } from "drizzle-orm";
 
-//TODO: IMPLEMENTAR HANDLER
 export const deleteTodo = new Elysia().delete(
   "/:id",
-  ({ params: { id } }) => `delete todo id: ${id}`,
+  async ({ params: { id } }) => {
+    await db.delete(todo).where(eq(todo.id, id))
+  },
   {
     params: t.Object({
         id: t.Numeric()
