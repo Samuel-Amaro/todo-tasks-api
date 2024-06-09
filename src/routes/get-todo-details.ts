0379f2d478c4361b516f48f43ea37f8eca421cb0
Samuel Amaro <samuelamaro96746313@gmail.com>
2024-06-09 15:41:58 -0300

    feat: adiciona `endpoint get-todo-details`

--- src/routes/get-todo-details.ts
+++ src/routes/get-todo-details.ts
@@ -1,9 +1,21 @@
 import { Elysia, t } from "elysia";
+import { db } from "../database/connection";
+import { todo } from "../database/schema";
+import { eq } from "drizzle-orm";
 
-//TODO: IMPLEMENTAR HANDLER
 export const getTodoDetails = new Elysia().get(
   "/:id",
-  ({ params: { id } }) => `get details todo id: ${id}`, 
+  async ({ params: { id }, set }) => {
+    const result = await db.select().from(todo).where(eq(todo.id, id))
+    if(result.length === 0) {
+      set.status = 404
+      return {
+        code: "NOT_FOUND",
+        message: "Todo não encontrado"
+      }
+    }
+    return result 
+  }, 
   {
     params: t.Object({
         id: t.Numeric()
