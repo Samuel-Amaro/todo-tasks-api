6b48a155bdec43f2a79f6692b5365dd4b7dbf7ac
Samuel Amaro <samuelamaro96746313@gmail.com>
2024-06-09 13:35:18 -0300

    feat: adiciona endpoint `update todo` with method `http put`

--- src/routes/update-todo.ts
+++ src/routes/update-todo.ts
@@ -1,14 +1,37 @@
 import { Elysia, t } from "elysia";
+import { db } from "../database/connection";
+import { todo, statusEnum } from "../database/schema";
+import { eq } from "drizzle-orm";
 
-//TODO: IMPLEMENTAR HANDLER
+const status = [...statusEnum.enumValues] as const;
+
+enum Status {
+  pending = "pending",
+  in_progress = "in_progress",
+  completed = "completed",
+}
+
 export const updateTodo = new Elysia().put(
   "/:id",
-  ({ params: { id }, body }) => `update todo id: ${id}`,
+  async ({ params: { id }, body, set }) => {
+    await db
+      .update(todo)
+      .set({
+        title: body.title,
+        description: body.content,
+        status: body.status,
+        updated_at: new Date(),
+      })
+      .where(eq(todo.id, id));
+  },
   {
     params: t.Object({ id: t.Numeric() }),
     body: t.Object({
-      title: t.Optional(t.String()),
+      title: t.String({
+        minLength: 1,
+      }),
       content: t.Optional(t.String()),
+      status: t.Optional(t.Enum(Status)),
     }),
   }
 );
